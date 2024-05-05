de34c902095ac5c15e9e2b4f01c7d3fa5065bf3f
M HASAN <meherajhasan2002@gmail.com>
2024-05-05 23:12:20 +0600

    Login Page UI Updated

--- WelcomeModel/FormLogIn.cs
+++ WelcomeModel/FormLogIn.cs
@@ -9,31 +9,22 @@
 
         private void buttonClear_Click(object sender, EventArgs e)
         {
-            textBoxUserName.Text = string.Empty;
             textBoxPassword.Text = string.Empty;
-            textBoxUserName.Focus();
+            textBoxUsername.Text = string.Empty;
+            textBoxPassword.Focus();
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBoxShowPassword.Checked)
+                textBoxPassword.UseSystemPasswordChar = false;
+            else
+                textBoxPassword.UseSystemPasswordChar = true;
         }
 
         private void buttonClose_Click(object sender, EventArgs e)
         {
             Application.Exit();
-        }
-
-        private void checkBox1_Click(object sender, EventArgs e)
-        {
-            if (checkBox1.Checked)
-            {
-                textBoxPassword.PasswordChar = '\0';
-            }
-            if (!checkBox1.Checked)
-            {
-                textBoxPassword.PasswordChar = '*';
-            }
         }
     }
 }
