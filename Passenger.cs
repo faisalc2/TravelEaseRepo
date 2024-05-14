81b783cd42e4500310e97fb1a1df702b68927596
M HASAN <meherajhasan2002@gmail.com>
2024-05-14 21:04:25 +0600

    backEnd_Bus

--- Passenger.cs
+++ Passenger.cs
@@ -2,5 +2,8 @@
 {
     internal class Passenger : User
     {
+        public string passengerID {  get; set; }
+        public string passengerName { get; set;}
+        public string passengerGender { get; set;}
     }
 }
