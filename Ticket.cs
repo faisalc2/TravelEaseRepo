131e1bf0454104909a0dd88e0d952dcbccd3c2b4
M HASAN <meherajhasan2002@gmail.com>
2024-05-15 22:40:45 +0600

    with Error

--- Ticket.cs
+++ Ticket.cs
@@ -6,12 +6,21 @@ using System.Threading.Tasks;
 
 namespace TravelEase
 {
-    internal class Ticket
+    public class Ticket
     {
         private string _serialNumber;
         private string _passengerInfo;
         private int _seatNumber;
         private int _fair;
         private int _seatAmount;
+
+        //public Ticket ()
+
+        public string SerialNumber { get; set; }
+        public string PassengerInfo { get; set; }
+        public int SeatNumber { get; set; }
+        public int Fare { get; set; }
+        public int SeatAmount { get; set; }
+
     }
 }
