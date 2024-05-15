c593ed49b26fc8ed17146fe2ffe3a888c7dc0b16
Foisal <foisalc2@gmail.com>
2024-05-15 20:30:48 +0600

    Passenger gridline and other ui changed

--- PassengerDashboards/PassengerHistory.Designer.cs
+++ PassengerDashboards/PassengerHistory.Designer.cs
@@ -28,51 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
             label1 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            DGVRecentTicketInfo = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)DGVRecentTicketInfo).BeginInit();
             SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.AllowUserToAddRows = false;
-            dataGridView1.AllowUserToDeleteRows = false;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(133, 88);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.ReadOnly = true;
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(562, 383);
-            dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonHighlight;
             label1.Location = new Point(302, 30);
             label1.Name = "label1";
-            label1.Size = new Size(155, 28);
+            label1.Size = new Size(173, 31);
             label1.TabIndex = 1;
             label1.Text = "Recent Ticktes:";
+            // 
+            // DGVRecentTicketInfo
+            // 
+            DGVRecentTicketInfo.AllowUserToAddRows = false;
+            DGVRecentTicketInfo.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(48, 56, 79);
+            DGVRecentTicketInfo.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
+            DGVRecentTicketInfo.BorderStyle = BorderStyle.None;
+            DGVRecentTicketInfo.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = Color.FromArgb(48, 56, 79);
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle4.ForeColor = Color.White;
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            DGVRecentTicketInfo.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            DGVRecentTicketInfo.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DGVRecentTicketInfo.EnableHeadersVisualStyles = false;
+            DGVRecentTicketInfo.Location = new Point(77, 115);
+            DGVRecentTicketInfo.Name = "DGVRecentTicketInfo";
+            DGVRecentTicketInfo.ReadOnly = true;
+            DGVRecentTicketInfo.RowHeadersWidth = 51;
+            DGVRecentTicketInfo.Size = new Size(602, 342);
+            DGVRecentTicketInfo.TabIndex = 2;
             // 
             // PassengerHistory
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(48, 56, 79);
             ClientSize = new Size(725, 538);
+            Controls.Add(DGVRecentTicketInfo);
             Controls.Add(label1);
-            Controls.Add(dataGridView1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "PassengerHistory";
             Text = "PassengerHistory";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DGVRecentTicketInfo).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private DataGridView dataGridView1;
         private Label label1;
+        private DataGridView DGVRecentTicketInfo;
     }
 }
